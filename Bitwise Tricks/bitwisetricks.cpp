4c6bfe02665a925a5b6e9a9271da054a889c5f08
Shreya Prasad <shreyapd06@gmail.com>
2021-02-14 15:14:14 +0530

    Update bitwisetricks.cpp

--- Bitwise Tricks/bitwisetricks.cpp
+++ Bitwise Tricks/bitwisetricks.cpp
@@ -1,7 +1,19 @@
-  // How to set nth position in number ‘num’ :   
+  // How to set nth position in number ‘num’ : Observations: It adds num by 2 
      
      void set(int & num,int n) 
 {    
      int shift = 1<< n;  // First step is shift '1', 
      num = num | shift ;  // second  step is bitwise OR 
 } 
+
+
+
+// How to unset/clear a bit at n’th position in the number ‘num’. Observation: It subtracts num by 2.
+
+    void unset(int & num,int n) 
+{ 
+     // First step is shift '1', second 
+     // step is bitwise OR 
+     int shift = 1<< n;
+     num = num & (~shift) ; 
+}
